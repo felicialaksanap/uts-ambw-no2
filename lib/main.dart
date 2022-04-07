9c16da646b7b30d89258fb05afecea777b11c75b
felicialaksanap <79403318+felicialaksanap@users.noreply.github.com>
2022-04-07 15:29:44 +0700

    continue learning

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -66,6 +67,138 @@ class MyApp extends StatelessWidget {
                         Icon(Icons.design_services),
                         Text("Design",style: TextStyle(fontSize: 12.0, color: Colors.grey),),
                       ],
+                    ),
+                  ],
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: Row(
+                  children: const[
+                    Text(
+                      "Continue Learning : ",
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+                    ),
+                  ],
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      color: Colors.green[100],
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const[
+                              Icon(Icons.date_range_outlined),
+                              SizedBox(height: 8.0),
+                              Text("Science",style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold),),
+                              Text("Chapter 4",style: TextStyle(fontSize: 12.0, color: Colors.grey, fontWeight: FontWeight.bold),),
+                              SizedBox(height: 16.0),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: const[
+                              Icon(Icons.alarm, color: Colors.grey),
+                              SizedBox(width: 8.0),
+                              Text("27 Mins",style: TextStyle(fontSize: 12.0, color: Colors.grey),),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+
+                    Container(
+                      color: Colors.green[100],
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const[
+                              Icon(Icons.design_services),
+                              SizedBox(height: 8.0),
+                              Text("Design",style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold),),
+                              Text("Chapter 3",style: TextStyle(fontSize: 12.0, color: Colors.grey, fontWeight: FontWeight.bold),),
+                              SizedBox(height: 16.0),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: const[
+                              Icon(Icons.alarm, color: Colors.grey),
+                              SizedBox(width: 8.0),
+                              Text("30 Mins",style: TextStyle(fontSize: 12.0, color: Colors.grey),),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+
+                    Container(
+                      color: Colors.green[100],
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const[
+                              Icon(Icons.biotech_outlined),
+                              SizedBox(height: 8.0),
+                              Text("Biology",style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold),),
+                              Text("Chapter 1",style: TextStyle(fontSize: 12.0, color: Colors.grey, fontWeight: FontWeight.bold),),
+                              SizedBox(height: 16.0),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: const[
+                              Icon(Icons.alarm, color: Colors.grey),
+                              SizedBox(width: 8.0),
+                              Text("25 Mins",style: TextStyle(fontSize: 12.0, color: Colors.grey),),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+
+                    Container(
+                      color: Colors.green[100],
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const[
+                              Icon(Icons.coffee),
+                              SizedBox(height: 8.0),
+                              Text("Cooking",style: TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold),),
+                              Text("Chapter 3",style: TextStyle(fontSize: 12.0, color: Colors.grey, fontWeight: FontWeight.bold),),
+                              SizedBox(height: 16.0),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: const[
+                              Icon(Icons.alarm, color: Colors.grey),
+                              SizedBox(width: 8.0),
+                              Text("18 Mins",style: TextStyle(fontSize: 12.0, color: Colors.grey),),
+                            ],
+                          )
+                        ],
+                      ),
                     ),
                   ],
                 ),
